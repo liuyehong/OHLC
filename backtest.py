e1094dfcff08de59bb28b6e51a8b662d1e6b52ef
Liu Yehong <liuyehong@users.noreply.github.com>
2019-11-22 18:02:42 +0800

    Add files via upload

--- backtest.py
+++ backtest.py
@@ -11,31 +11,25 @@ class Backtest:
         self.cost = 0.001
         self.freq = '1Min'
 
+
     def run(self, strategy):
-        self.history_asset = []
-        self.buy_time = []
-        self.sell_time = []
-        self.history_inventory = []
-        cash = 1
-        stock = 0
+        self.cash = 1
+        self.stock = 0
 
         cond_b, cond_s, price_b, price_s = strategy
 
-        for t in range(1, self.alpha.T-1):
-            if cond_b[t] and stock == 0 and price_b[t] > self.alpha.low[t+1]:
-                stock = cash / price_b[t] * (1 - self.cost)
-                cash = 0
-                self.buy_time.append(t)
+        def step(t):
+            if cond_b[t] and self.stock == 0 and price_b[t] > self.alpha.low[t + 1]:
+                self.stock = self.cash / price_b[t] * (1 - self.cost)
+                self.cash = 0
 
-            elif cond_s[t] and cash == 0 and price_s[t] < self.alpha.high[t+1]:
-                cash = stock * price_s[t] * (1 - self.cost)
-                stock = 0
-                self.sell_time.append(t)
+            elif cond_s[t] and self.cash == 0 and price_s[t] < self.alpha.high[t + 1]:
+                self.cash = self.stock * price_s[t] * (1 - self.cost)
+                self.stock = 0
 
-            asset = stock * self.alpha.close[t] + cash
-            self.history_asset.append(asset)
-            self.history_inventory.append(stock > 0)
-
+            return self.cash + self.stock*self.alpha.close[t+1]
+        #  backtest
+        self.history_asset = [step(t) for t in range(1, self.alpha.T-1)]
         self.r = np.diff(self.history_asset)/self.history_asset[:-1]
         self.sharpe_ratio = np.average(self.r)/np.std(self.r)
 
@@ -53,18 +47,9 @@ class Backtest:
             self.sharpe_ratio_year = self.sharpe_ratio * np.sqrt(240 * 250 / 60)
 
 
-
         if np.isnan(self.sharpe_ratio):
             self.sharpe_ratio = -1
             self.sharpe_ratio_year = -1
-
-
-        df_close = pd.DataFrame({'asset': self.history_asset})['asset']
-        rollmax = df_close.cummax()
-        daily_drawdown = df_close/rollmax - 1
-        self.max_drawdown = np.min(daily_drawdown)
-
-
 
 
 if __name__ == '__main__':
